59a1d8b4783ac23d199a8397803f768f741ec840
flavifaria <lefravious@gmail.com>
2023-11-22 09:33:28 -0300

    update 22-11

--- JavaScript/aula.js
+++ JavaScript/aula.js
@@ -1,1 +1,16 @@
-console.log("imprimir algo");
+/*Boolean
+ -Igualdade ===
+ -
+ -
+ -   
+*/ 
+const numero = 0 ;
+const numeroPar = (numero % 2)===0;
+
+if (numero === 0){
+    console.log("Numero Inválido !")
+    } else if(numeroPar){
+        console.log("É um numero par !")
+            }else{
+                console.log("É um numero impar !")
+            }
