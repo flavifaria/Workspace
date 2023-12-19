d627ae8d82c165fb4e5df768cc2bbca52824dc33
flavifaria <lefravious@gmail.com>
2023-12-19 12:04:50 -0300

    update

---
+++ Jogos/Memoria/scr/script/engine.js
@@ -0,0 +1,27 @@
+const emojis = [
+    '😊',
+    '😊',
+    '🤞',
+    '🤞',
+    '💖',
+    '💖',
+    '🤢',
+    '🤢',
+    '😆',
+    '😆',
+    '👽',
+    '👽',
+    '💩',
+    '💩'
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(()=>(Math.random() > 0.5)? 2: -1)
+
+for(let i=0 ; i < emojis.length;i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
